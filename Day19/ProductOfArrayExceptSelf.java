d381b9de6551cfe5a63d2389a10d8c9b6fcc8a94
Anamika-1629 <anamikarai835@gmail.com>
2026-04-02 23:47:18 +0530

    Day 19 - 238. Product of Array Except Self

---
+++ Day19/ProductOfArrayExceptSelf.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [] nums = new int[n];
+
+        for (int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        ProductOfArrayExceptSelf obj = new ProductOfArrayExceptSelf();
+        int[] res = obj.productExceptSelf(nums);
+
+        for (int i = 0; i < res.length; i++){
+            System.out.println(res[i]);
+        }
+
+        sc.close();
+    }
+}
