67172aa2d57e41ae5fd5284cc60d246173fa5ae2
Anamika-1629 <anamikarai835@gmail.com>
2026-07-03 23:57:06 +0530

    Day 45 - 4. Median of Two Sorted Arrays

---
+++ Day45/MedianOfTwoSortedArray_MergeArrays.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution{
+    public double findMedianSortedArrays(int[] nums1, int[] nums2){
+        List<Integer> merged = new ArrayList<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            merged.add(nums1[i]);
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            merged.add(nums2[i]);
+        }
+
+        Collections.sort(merged);
+
+        if (merged.size() % 2 != 0) {
+            int med = merged.size() / 2;
+            return merged.get(med);
+        }
+
+        int m1 = merged.size() / 2 - 1;
+        int m2 = merged.size() / 2;
+
+        return (merged.get(m1) + merged.get(m2)) / 2.0;
+    }
+}
+
+public class MedianOfTwoSortedArray_MergeArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n1 = sc.nextInt();
+        int[] nm1 = new int[n1];
+        for (int i = 0; i < n1; i++) nm1[i] = sc.nextInt();
+
+        int n2 = sc.nextInt();
+        int[] nm2 = new int[n2];
+        for (int i = 0; i < n2; i++) nm2[i] = sc.nextInt();
+
+        Solution sol = new Solution();
+        System.out.println(sol.findMedianSortedArrays(nm1, nm2));
+        sc.close();
+    }
+}
