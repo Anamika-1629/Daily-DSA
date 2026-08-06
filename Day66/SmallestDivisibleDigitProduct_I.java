196ccdfe20824ed2a4a12f6ed03f012c3179ca86
Anamika-1629 <anamikarai835@gmail.com>
2026-08-06 23:29:02 +0530

    Day 66 - 3345. Smallest Divisible Digit Product I

---
+++ Day66/SmallestDivisibleDigitProduct_I.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while (true) {
+            int product = 1;
+            int num = n;
+
+            while (num > 0) {
+                product *= (num % 10);
+                num /= 10;
+            }
+
+            if (product % t == 0) {
+                return n;
+            }
+
+            n++;
+        }
+    }
+}
+public class SmallestDivisibleDigitProduct_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+
+        Solution sol = new Solution();
+        System.out.println(sol.smallestNumber(n, t));
+        sc.close();
+    }    
+}
